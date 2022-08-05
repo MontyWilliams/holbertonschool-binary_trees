5b42282747faf408bb86cae87795ec654acab0c7
Montywilliams <3650@holbertonschool.com>
2022-08-04 20:37:45 -0600

    push all

---
+++ binary_tree_print.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "binary_trees.h"
+
+
+/**
+ * print_t - Stores
+  sgtrings
+ *
+ * @tree: Pointer to the node to print
+ * @offset: Offset to print
+ * @depth: Depth of the node
+ * @s: Buffer
+ *
+ * Return: length of printed tree after process
+ */
+
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
+{
+
+}
